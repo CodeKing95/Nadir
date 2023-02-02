d4d80fa095eeccf2bbb7aa550f8da9918c125852
Nadir Ahmed <102181168+CodeKing95@users.noreply.github.com>
2023-02-02 13:14:47 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,74 @@
-//active navbar
-let nav = document.querySelector(".navigation-wrap");
-window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
-        nav.classList.add("scroll-on");
-    }else{
-        nav.classListner.remove("scroll-on");
-    }
-}
+window.addEventListener('load', () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
 
-// nav hide
-let navBar = document.querySelectorAll('.nav-link');
-let navCollapse = document.querySelector('.navbar-collapse.collapse');
-navBar.forEach(function(a){
-    a.addEventListener("click", function(){
-        navCollapse.classList.remove("show");
-    })
-})
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-//counter design
-document.addEventListener("DOMContentLoaded", () => {
-    function counter(id, start, end, duration){
-        let obj = document.getElementById(id),
-        current = start,
-        range = end - start,
-        increment = end > start ? 1: -1,
-        step = Math.abs(Math.floor(duration / range)),
-        timer = setInterval(() => {
-            current += increment;
-            obj.textContent = current;
-            if(current == end){
-                clearInterval(timer);
+        const task = input.value;
+
+        if (!task) {
+            alert("Please fill out the task force details");
+            return;
+        }
+
+        const task_el = document.createElement("div");
+        task_el.classList.add("task");
+
+        const task_content_el = document.createElement("div");
+        task_content_el.classList.add("content");
+        
+        task_el.appendChild(task_content_el);
+
+        
+
+        const task_input_el = document.createElement("input");
+        task_input_el.classList.add("text");
+        task_input_el.type = "text";
+        task_input_el.value = task;
+        task_input_el.setAttribute("readonly", "readonly");
+
+        task_content_el.appendChild(task_input_el);
+
+        const task_actions_el= document.createElement("div");
+        task_actions_el.classList.add("actions");
+
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML = "Edit";
+
+        const task_delete_el = document.createElement
+        ("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerHTML = "Delete";
+
+        task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
+
+        task_el.appendChild(task_actions_el);
+
+
+        list_el.appendChild(task_el);
+
+        input.value = "";
+
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLowerCase() ==
+            "edit") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                task_edit_el.innerText = "Save";
+            } else {
+                task_input_el.setAttribute("readonly",
+                "readonly");
+                task_edit_el.innerText = "Edit";
             }
-        }, step);
-    }
-    counter("count1", 0, 1287, 3000);
-    counter("count2", 100, 5786, 2500);
-    counter("count3", 0, 1440, 3000);
-    counter("count4", 0, 7110, 3000);
+        });
+
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el);
+
+        });
+    });
 });
